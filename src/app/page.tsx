c276ff754bafb514a24dd1d9b155b0b19e6bdd62
moojing <starts789@gmail.com>
2023-10-15 23:30:33 +0800

    feat: get data from google spread sheet.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+import { useEffect } from 'react'
 import Image from 'next/image'
+import PublicGoogleSheetsParser from 'public-google-sheets-parser'
 import styles from './page.module.css'
+const spreadsheetId = '1j4pziNq3RI1slNrBapJsDUtZ0n1qJX8_P832mPYQ3PM';
 
 export default function Home() {
+
+  useEffect(() => {
+    new PublicGoogleSheetsParser().parse(spreadsheetId).then((data) => {
+      console.log(data);
+    })
+  }, [])
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
