5aba3a6f27ee110d9748ebaf757679af024b3ea4
stan <tonytonitone6@gmail.com>
2023-11-23 18:25:41 +0800

    feat: button component and i18n btn

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	useTabs: true,
+};
+
