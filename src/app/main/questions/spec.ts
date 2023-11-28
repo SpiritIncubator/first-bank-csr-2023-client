0c20afc29d7fa1d7a01a033a4e0c88f40c30f1ef
Stan <tonytonitone6@gmail.com>
2023-11-29 01:29:05 +0800

    feat: add question Q&A mechanism and add zustand as global state management pkg

---
+++ src/app/main/questions/spec.ts
@@ -0,0 +1,168 @@
+type OptionType = {
+	content: string;
+	score: number;
+};
+
+type LevelType = {
+	imgSrc: string;
+	description: string;
+	options: OptionType[];
+};
+
+export const questionList: Array<LevelType> = [
+	{
+		imgSrc: '',
+		description:
+			'自藝術之美延伸生態、歷史、文化相關創作、展演、娛樂者皆類相關創作、展演、娛樂者皆類。',
+		options: [
+			{
+				content: 'first1',
+				score: 0,
+			},
+			{
+				content: 'second1',
+				score: 0,
+			},
+			{
+				content: 'three1',
+				score: 0,
+			},
+			{
+				content: 'four1',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '',
+		options: [
+			{
+				content: 'first2',
+				score: 0,
+			},
+			{
+				content: 'second2',
+				score: 0,
+			},
+			{
+				content: 'three2',
+				score: 0,
+			},
+			{
+				content: 'four2',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '',
+		options: [
+			{
+				content: 'first3',
+				score: 0,
+			},
+			{
+				content: 'second3',
+				score: 0,
+			},
+			{
+				content: 'three3',
+				score: 0,
+			},
+			{
+				content: 'four3',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '',
+		options: [
+			{
+				content: 'first4',
+				score: 0,
+			},
+			{
+				content: 'second4',
+				score: 0,
+			},
+			{
+				content: 'three4',
+				score: 0,
+			},
+			{
+				content: 'four4',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '',
+		options: [
+			{
+				content: 'first5',
+				score: 0,
+			},
+			{
+				content: 'second5',
+				score: 0,
+			},
+			{
+				content: 'three5',
+				score: 0,
+			},
+			{
+				content: 'four5',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '6',
+		options: [
+			{
+				content: 'first6',
+				score: 0,
+			},
+			{
+				content: 'second6',
+				score: 0,
+			},
+			{
+				content: 'three6',
+				score: 0,
+			},
+			{
+				content: 'four6',
+				score: 0,
+			},
+		],
+	},
+	{
+		imgSrc: '',
+		description: '7',
+		options: [
+			{
+				content: 'first7',
+				score: 0,
+			},
+			{
+				content: 'second7',
+				score: 0,
+			},
+			{
+				content: 'three7',
+				score: 0,
+			},
+			{
+				content: 'four7',
+				score: 0,
+			},
+		],
+	},
+];
