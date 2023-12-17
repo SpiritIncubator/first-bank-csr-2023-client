688f1ed3df406090eae848f1e989f5618c19c268
moojing <starts789@gmail.com>
2023-12-17 16:59:38 +0800

    feat: finish final page for sending image.

--- src/app/messageWall/sendMessage/page.tsx
+++ src/app/messageWall/sendMessage/page.tsx
@@ -5,8 +5,10 @@ import { Box, Button, Container } from '@mui/material';
 import ColorPicker from '@/components/ColorPicker';
 import colors from '@/constants/colors';
 import ErrorModal from './components/ErrorModal'
+import FinalPage from './components/FinalPage'
 import ConfirmSubmit from './components/ConfirmSubmit'
 import { useTheme } from '@mui/material/styles';
+import { createMessage } from '@/api/index';
 
 export default function SendMessage() {
   const [name, setName] = React.useState('');
@@ -14,7 +16,7 @@ export default function SendMessage() {
   const [noteColor, setNoteColor] = React.useState('#F8E47E');
   const [errorMessage, setErrorMessage] = React.useState('');
   const [readyToSubmit, setReadyToSubmit] = React.useState(false);
-  const [finishSubmit, setFinishSubmit] = React.useState(false);
+  const [finishSubmit, setFinishSubmit] = React.useState(true);
   const theme = useTheme();
 
   const mobileContainerStyle = {
@@ -48,233 +50,245 @@ export default function SendMessage() {
     setErrorMessage('')
   }
 
-  const onConfirmSubmit = () => {
+  const onConfirmSubmit = async () => {
     console.log('onConfirmSubmit :', onConfirmSubmit);
     //@todo: api for submitting message 
     setFinishSubmit(true)
+
+    const result = await createMessage({
+      name,
+      message,
+      color: noteColor
+    })
+    console.log('result :', result);
   }
 
   return <>
-    <Box sx={{
-      ...readyToSubmit && !finishSubmit ? {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        bgcolor: 'white',
-        width: '100%',
-        height: '100%',
-        zIndex: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      } : {
-        display: 'none'
-      }
-    }}>
-      <ConfirmSubmit
-        noteColor={noteColor}
-        name={name}
-        message={message}
-        onReject={() => setReadyToSubmit(false)}
-        onConfirm={onConfirmSubmit} />
-    </Box>
-    <Container maxWidth="sm" sx={mobileContainerStyle}>
-      <Box sx={{
-        width: "230px",
-        height: "240px",
-        margin: '0 auto',
-        overflow: 'hidden',
-        position: 'relative'
-      }}>
-        <Image
-          src="/assets/bird_books.png"
-          alt="bird_books"
-          objectFit='contain'
-          fill
-          style={{ marginBottom: "16px" }} />
-      </Box>
-      <Box sx={{
-        width: "250px",
-        height: "60px",
-        margin: '0 auto',
-        overflow: 'hidden',
-        position: 'relative'
-      }}>
-        <Image
-          src="/assets/sendMessage_title.svg"
-          alt="sendMessage_title"
-          objectFit='contain'
-          fill
-          style={{ marginBottom: "16px" }} />
-      </Box>
-      <Box sx={{
-        width: "240px",
-        height: "45px",
-        margin: '0 auto',
-        overflow: 'hidden',
-        position: 'relative'
-      }}>
-        <Image
-          src="/assets/sendMessage_subTitle.svg"
-          alt="sendMessage_subTitle"
-          objectFit='contain'
-          fill
-          style={{ marginTop: "-12px" }} />
-      </Box>
-
-      <Box sx={{
-        width: "342px",
-        height: "5px",
-        margin: '0 auto',
-        overflow: 'hidden',
-        position: 'relative',
-        marginTop: "60px",
-        marginBottom: "20px"
-      }}>
-        <Image
-          src="/assets/divider.svg"
-          alt="divider"
-          objectFit='contain'
-          fill />
-      </Box>
-
-      <Box
-        component="form"
-        sx={{
-          width: '100%',
-          marginBottom: '40px',
-          '& .input-base': {
-            width: '100%',
-            padding: '20px 24px',
-            fontSize: '1rem',
-            borderRadius: '20px',
-            border: `2px solid ${colors.brown1}`,
-            backgroundColor: colors.brown,
-            margin: '8px 0',
-            boxSizing: 'border-box',
-            outline: 'none',
-            transition: 'border-color 0.2s ease-in-out',
-            letterSpacing: '1.28px',
-            '&:focus': {
-              border: `2px solid ${colors.brown4}`,
-            },
-            '&::placeholder': {
-              color: 'rgba(0, 0, 0, 0.6)',
-            },
-          },
-          '& .textarea-base': {
-            letterSpacing: '1.28px',
-            width: '100%',
-            padding: '20px 24px',
-            fontSize: '1rem',
-            borderRadius: '20px',
-            border: `2px solid ${colors.brown1}`,
-            backgroundColor: colors.brown,
-            resize: 'none',
-            boxSizing: 'border-box',
-            outline: 'none',
-            transition: 'border-color 0.2s ease-in-out',
-            '&:focus': {
-              border: `2px solid ${colors.brown4}`,
-            },
-          },
-          '& .input-base::placeholder, & .textarea-base::placeholder': {
-            color: colors.brown2, // Theme color for placeholder
-            opacity: 1, // Full opacity
-          },
-        }}
-      >
+    {finishSubmit ? < FinalPage /> : (
+      <>
         <Box sx={{
-          width: "202px",
-          height: "30px",
-          margin: '0 auto',
-          overflow: 'hidden',
-          position: 'relative',
-          marginBottom: "10px",
-          display: 'flex'
+          ...readyToSubmit ? {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            bgcolor: 'white',
+            width: '100%',
+            height: '100%',
+            zIndex: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          } : {
+            display: 'none'
+          }
         }}>
-          <Image
-            width="129"
-            height="38"
-            src="/assets/sendMessage_write.svg"
-            alt="sendMessage_write"
-            style={{ marginRight: '8px' }}
-          />
-          <Image
-            width="65"
-            height="38"
-            src="/assets/sendMessage_content.svg"
-            alt="sendMessage_content"
-
-          />
+          <ConfirmSubmit
+            noteColor={noteColor}
+            name={name}
+            message={message}
+            onReject={() => setReadyToSubmit(false)}
+            onConfirm={onConfirmSubmit} />
         </Box>
-        <textarea
-          placeholder="請寫下你會如何在生活中實踐永續，或是今天的觀展心得吧！（字數限制50字） "
-          className="textarea-base"
-          rows={4}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <Box sx={{
-          width: "202px",
-          height: "30px",
-          margin: '0 auto',
-          overflow: 'hidden',
-          position: 'relative',
-          marginBottom: "10px",
-          marginTop: "30px",
-          display: 'flex'
-        }}>
-          <Image
-            width="129"
-            height="38"
-            src="/assets/sendMessage_writeName.svg"
-            alt="sendMessage_writeName"
-            style={{ marginRight: '8px' }}
+        <Container maxWidth="sm" sx={mobileContainerStyle}>
+          <Box sx={{
+            width: "230px",
+            height: "240px",
+            margin: '0 auto',
+            overflow: 'hidden',
+            position: 'relative'
+          }}>
+            <Image
+              src="/assets/bird_books.png"
+              alt="bird_books"
+              objectFit='contain'
+              fill
+              style={{ marginBottom: "16px" }} />
+          </Box>
+          <Box sx={{
+            width: "250px",
+            height: "60px",
+            margin: '0 auto',
+            overflow: 'hidden',
+            position: 'relative'
+          }}>
+            <Image
+              src="/assets/sendMessage_title.svg"
+              alt="sendMessage_title"
+              objectFit='contain'
+              fill
+              style={{ marginBottom: "16px" }} />
+          </Box>
+          <Box sx={{
+            width: "240px",
+            height: "45px",
+            margin: '0 auto',
+            overflow: 'hidden',
+            position: 'relative'
+          }}>
+            <Image
+              src="/assets/sendMessage_subTitle.svg"
+              alt="sendMessage_subTitle"
+              objectFit='contain'
+              fill
+              style={{ marginTop: "-12px" }} />
+          </Box>
+
+          <Box sx={{
+            width: "342px",
+            height: "5px",
+            margin: '0 auto',
+            overflow: 'hidden',
+            position: 'relative',
+            marginTop: "60px",
+            marginBottom: "20px"
+          }}>
+            <Image
+              src="/assets/divider.svg"
+              alt="divider"
+              objectFit='contain'
+              fill />
+          </Box>
+
+          <Box
+            component="form"
+            sx={{
+              width: '100%',
+              marginBottom: '40px',
+              '& .input-base': {
+                width: '100%',
+                padding: '20px 24px',
+                fontSize: '1rem',
+                borderRadius: '20px',
+                border: `2px solid ${colors.brown1}`,
+                backgroundColor: colors.brown,
+                margin: '8px 0',
+                boxSizing: 'border-box',
+                outline: 'none',
+                transition: 'border-color 0.2s ease-in-out',
+                letterSpacing: '1.28px',
+                '&:focus': {
+                  border: `2px solid ${colors.brown4}`,
+                },
+                '&::placeholder': {
+                  color: 'rgba(0, 0, 0, 0.6)',
+                },
+              },
+              '& .textarea-base': {
+                letterSpacing: '1.28px',
+                width: '100%',
+                padding: '20px 24px',
+                fontSize: '1rem',
+                borderRadius: '20px',
+                border: `2px solid ${colors.brown1}`,
+                backgroundColor: colors.brown,
+                resize: 'none',
+                boxSizing: 'border-box',
+                outline: 'none',
+                transition: 'border-color 0.2s ease-in-out',
+                '&:focus': {
+                  border: `2px solid ${colors.brown4}`,
+                },
+              },
+              '& .input-base::placeholder, & .textarea-base::placeholder': {
+                color: colors.brown2, // Theme color for placeholder
+                opacity: 1, // Full opacity
+              },
+            }}
+          >
+            <Box sx={{
+              width: "202px",
+              height: "30px",
+              margin: '0 auto',
+              overflow: 'hidden',
+              position: 'relative',
+              marginBottom: "10px",
+              display: 'flex'
+            }}>
+              <Image
+                width="129"
+                height="38"
+                src="/assets/sendMessage_write.svg"
+                alt="sendMessage_write"
+                style={{ marginRight: '8px' }}
+              />
+              <Image
+                width="65"
+                height="38"
+                src="/assets/sendMessage_content.svg"
+                alt="sendMessage_content"
+
+              />
+            </Box>
+            <textarea
+              placeholder="請寫下你會如何在生活中實踐永續，或是今天的觀展心得吧！（字數限制50字） "
+              className="textarea-base"
+              rows={4}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Box sx={{
+              width: "202px",
+              height: "30px",
+              margin: '0 auto',
+              overflow: 'hidden',
+              position: 'relative',
+              marginBottom: "10px",
+              marginTop: "30px",
+              display: 'flex'
+            }}>
+              <Image
+                width="129"
+                height="38"
+                src="/assets/sendMessage_writeName.svg"
+                alt="sendMessage_writeName"
+                style={{ marginRight: '8px' }}
+              />
+              <Image
+                width="45"
+                height="38"
+                src="/assets/sendMessage_name.svg"
+                alt="sendMessage_name"
+
+              />
+            </Box>
+            <input
+              type="text"
+              placeholder="請留下你的大名吧！"
+              className="input-base"
+              onChange={(e) => setName(e.target.value)}
+            />
+            {/* Continue with your form elements and submit button */}
+          </Box>
+
+          <ColorPicker
+            defaultColor="#F8E47E"
+            onColorChange={setNoteColor}
           />
-          <Image
-            width="45"
-            height="38"
-            src="/assets/sendMessage_name.svg"
-            alt="sendMessage_name"
 
-          />
-        </Box>
-        <input
-          type="text"
-          placeholder="請留下你的大名吧！"
-          className="input-base"
-          onChange={(e) => setName(e.target.value)}
-        />
-        {/* Continue with your form elements and submit button */}
-      </Box>
+          <Box sx={{
+            width: "200px",
+            height: "67px",
+            margin: '0 auto',
+            overflow: 'hidden',
+            position: 'relative',
+            marginTop: "50px",
+            cursor: 'pointer'
+          }}
+            onClick={onSubmit}>
+            <Image
+              src="/assets/sendMessage_confirm.svg"
+              alt="divider"
+              fill />
+          </Box>
 
-      <ColorPicker
-        defaultColor="#F8E47E"
-        onColorChange={setNoteColor}
-      />
-
-      <Box sx={{
-        width: "200px",
-        height: "67px",
-        margin: '0 auto',
-        overflow: 'hidden',
-        position: 'relative',
-        marginTop: "50px",
-        cursor: 'pointer'
-      }}
-        onClick={onSubmit}>
-        <Image
-          src="/assets/sendMessage_confirm.svg"
-          alt="divider"
-          fill />
-      </Box>
-
-      {errorMessage &&
-        <ErrorModal
-          message={errorMessage}
-          open={!!errorMessage}
-          onConfirm={onConfirmError} />}
-    </Container >
+          {errorMessage &&
+            <ErrorModal
+              message={errorMessage}
+              open={!!errorMessage}
+              onConfirm={onConfirmError} />}
+        </Container >
+      </>
+    )
+    }
   </>
 }
 
