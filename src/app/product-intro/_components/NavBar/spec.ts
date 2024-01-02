d753dee5d17b46bdfc34bc01682664daca7bd708
stan <tonytonitone6@gmail.com>
2024-01-02 18:19:05 +0800

    feat: navlink

---
+++ src/app/product-intro/_components/NavBar/spec.ts
@@ -0,0 +1,24 @@
+import { LinkType } from "./types";
+
+export const navigationLinksConfig: Array<LinkType> = [
+	{
+		label: 'home',
+		path: '/',
+	},
+	{
+		label: '永續績效連結授信專案',
+		path: '/path1',
+	},
+	{
+		label: '永續績效連結授信專案',
+		path: '/path2',
+	},
+	{
+		label: '永續績效連結授信專案',
+		path: '/path3',
+	},
+	{
+		label: '綠色債券與太陽能基金',
+		path: '/path4',
+	},
+];
