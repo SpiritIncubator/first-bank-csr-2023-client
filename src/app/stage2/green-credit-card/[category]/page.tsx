b89e2c08edcd658e443bc26cefd57dc24220251a
stan <tonytonitone6@gmail.com>
2024-01-22 15:10:39 +0800

    feat: revise layout to add customize section

--- src/app/stage2/green-credit-card/[category]/page.tsx
+++ src/app/stage2/green-credit-card/[category]/page.tsx
@@ -10,8 +10,13 @@ import ImageButton from '@/app/_components/ImageButton/ImageButton';
 import FadeIn from '@/app/_components/Transitions/FadeIn';
 import BackLeftButton from '@/assets/back_left.svg';
 import BackLeftActiveButton from '@/assets/back_left_active.svg';
-import { cardDetailWithType } from './spec';
+import {useTranslation} from '@/app/_locales/hooks/useTranslation';
+
+
+import { creditCardInfos } from './spec';
 import SymbolWithYears from './assets/symbol-years.svg';
+import LastSymbolWithYears from './assets/world-card-bird.svg';
+import ReleaseDate from './assets/world-release-date.svg';
 import Card from './assets/life-card-detail.svg';
 
 
@@ -22,8 +27,13 @@ type PageProps = {
 }
 
 const Page = ({ params }: PageProps) => {
-  const detail = cardDetailWithType[Number(params.category)];
   const router = useRouter();
+  const { t } = useTranslation('stage2');
+  const detail = creditCardInfos[Number(params.category) - 1];
+  const isLastCard = Number(params.category) === creditCardInfos.length;
+  const categoryColor = isLastCard ? 'rgba(238, 182, 144, 1)' : '#BBC318';
+  const symbolImg = isLastCard ? LastSymbolWithYears : SymbolWithYears
+  const imgSrc = detail.imgSrc['zh'];
 
   function renderCardDetail(description: string, index: number) {
     const isHeadRecord = index === 0;
@@ -31,9 +41,9 @@ const Page = ({ params }: PageProps) => {
       <FadeIn delay={(index + 1) * 0.3 + 0.5}>
         <ListItem sx={{ marginTop: isHeadRecord ? 0 : 20 }}>
           <ListItemIcon sx={{ marginRight: 10 }}>
-            <Typography width={70} height={70} borderRadius={35} bgcolor="#BBC318" />
+            <Typography width={70} height={70} borderRadius={35} bgcolor={categoryColor} />
           </ListItemIcon>
-          <ListItemText primaryTypographyProps={{ fontSize: 60, width: 1558, lineHeight: 2, letterSpacing: 8 }} primary={description} />
+          <ListItemText primaryTypographyProps={{ fontSize: 60, width: 1558, lineHeight: 2, letterSpacing: 6 }} primary={t(description)} />
         </ListItem>
       </FadeIn>
     )
@@ -50,7 +60,6 @@ const Page = ({ params }: PageProps) => {
   return (
     <Box
       pt={16.25}
-      // padding={[13.75, 29.75, 0, 28.628]}
       height="100%"
       display="flex"
       flexDirection="column"
@@ -58,13 +67,22 @@ const Page = ({ params }: PageProps) => {
       alignItems="center"
       position="relative"
     >
-      <Box>
-        <Image src={Card} alt="life-card" />
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Image src={imgSrc.titleImg} alt="title" /> 
+        <Box mt={10}>
+          <Image src={detail.src} alt="card" />
+        </Box>
+        <Box mt={12.5}>
+          <Image src={imgSrc.cardNameImg} alt="card-name" />
+        </Box>
+        <Box mt={5.5}>
+          <Image src={imgSrc.releaseDateImg} alt="release-date" />
+        </Box>
       </Box>
       <Box height={624} width="100%" display="flex" mt={23}>
         <FadeIn delay={0.5} flex={1} display='flex'>
           <Box flex={1} textAlign="right" pr={16.25}>
-            <Image src={SymbolWithYears} alt="years" />
+            <Image src={symbolImg} alt="years" />
           </Box>
           <Box flex={1}>
             <Box>
@@ -72,13 +90,13 @@ const Page = ({ params }: PageProps) => {
                 <Typography fontWeight={700} fontSize={54}>流通卡數</Typography>
                 <Typography fontSize={30}>(張)</Typography>
               </Box>
-              <Typography fontSize={160} fontWeight={900} color="#BBC318">
-                <CountUp start={0} end={270787} duration={3} />
+              <Typography fontSize={160} fontWeight={900} color={categoryColor}>
+                <CountUp start={0} end={detail.circulationAmount} duration={3} />
               </Typography>
             </Box>
             <Box>
               <Typography fontSize={54} fontWeight={700}>累積提撥金額</Typography>
-              <Typography fontSize={120} fontWeight={900} color="#BBC318">156,787,000</Typography>
+              <Typography fontSize={120} fontWeight={900} color={categoryColor}>{new Intl.NumberFormat('en-IN').format(detail.accumulateAmount)}</Typography>
             </Box>
           </Box>
         </FadeIn>
--- src/app/stage2/green-credit-card/[category]/spec.ts
+++ src/app/stage2/green-credit-card/[category]/spec.ts
@@ -1,41 +1,131 @@
-type CardDetail = {
-	descriptions: string[];
-};
+import GreenReleaseDate from './assets/green-release-date.svg';
+import TaoyuanReleaseDate from './assets/taoyuan-release-date.svg';
+import YilanReleaseDate from './assets/yilan-release-date.svg';
+import LeezenReleaseDate from './assets/leezen-release-date.svg';
+import LastReleaseDate from './assets/world-release-date.svg';
 
-export const cardDetailWithType: Record<number, CardDetail> = {
-	0: {
-		descriptions: [
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-		],
-	},
-	1: {
-		descriptions: [
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-		],
-	},
-	2: {
-		descriptions: [
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-		],
-	},
-	3: {
-		descriptions: [
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-		],
-	},
-	4: {
-		descriptions: [
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-			'按國內刷卡消費金額2‰提撥予第一銀行文教基金會從事環境永續公益活動第一銀行文教。',
-		],
+import GreenCard from './assets/green-card.svg';
+import TaoyuanCard from './assets/taoyuan-card.svg';
+import YilanCard from './assets/yilan-card.svg';
+import LeezenCard from './assets/leezen-card.svg';
+import WorldCard from './assets/world-card.svg';
+
+import GreenCardTitle from './assets/green-card-title.svg';
+import TaoyuanCardTitle from './assets/taoyuan-card-title.svg';
+import YilanCardTitle from './assets/yilan-card-title.svg';
+import LeezenCardTitle from './assets/leezen-card-title.svg';
+import WorldCardTitle from './assets/world-card-title.svg';
+
+import GreenCardSample from './assets/green-card-sample.svg';
+import WorldCardSample from './assets/world-card-sample.svg';
+
+
+type LanguageType = 'en' | 'zh';
+
+type CreditCardInfoType = {
+	circulationAmount: number;
+	accumulateAmount: number;
+	descriptions: string[];
+	src: string;
+	imgSrc: {
+		[key in LanguageType]: {
+			titleImg: string;
+			releaseDateImg: string;
+			cardNameImg: string;
+		};
 	}
 };
+
+
+export const creditCardInfos: Array<CreditCardInfoType> = [
+	{
+		circulationAmount: 54439,
+		accumulateAmount: 7280711,
+		descriptions: ['green.card.detail.one'],
+		src: GreenCardSample,
+		imgSrc: {
+			en: {
+				titleImg: '',
+				releaseDateImg: '',
+				cardNameImg: '',
+			},
+			zh: {
+				titleImg: GreenCardTitle,
+				releaseDateImg: GreenReleaseDate,
+				cardNameImg: GreenCard,
+			},
+		},
+	},
+	{
+		circulationAmount: 113921,
+		accumulateAmount: 7077047,
+		descriptions: ['taoyuan.card.detail.one', 'taoyuan.card.detail.two'],
+		src: GreenCardSample,
+		imgSrc: {
+			en: {
+				titleImg: '',
+				releaseDateImg: '',
+				cardNameImg: '',
+			},
+			zh: {
+				titleImg: TaoyuanCardTitle,
+				releaseDateImg: TaoyuanReleaseDate,
+				cardNameImg: TaoyuanCard,
+			},
+		},
+	},
+	{
+		circulationAmount: 50584,
+		accumulateAmount: 10285527,
+		descriptions: ['yilan.card.detail.one', 'taoyuan.card.detail.two'],
+		src: GreenCardSample,
+		imgSrc: {
+			en: {
+				titleImg: '',
+				releaseDateImg: '',
+				cardNameImg: '',
+			},
+			zh: {
+				titleImg: YilanCardTitle,
+				releaseDateImg: YilanReleaseDate,
+				cardNameImg: YilanCard,
+			},
+		},
+	},
+	{
+		circulationAmount: 46453,
+		accumulateAmount: 132143734,
+		descriptions: ['leezen.card.detail.one', 'taoyuan.card.detail.two'],
+		src: GreenCardSample,
+		imgSrc: {
+			en: {
+				titleImg: '',
+				releaseDateImg: '',
+				cardNameImg: '',
+			},
+			zh: {
+				titleImg: LeezenCardTitle,
+				releaseDateImg: LeezenReleaseDate,
+				cardNameImg: LeezenCard,
+			},
+		},
+	},
+	{
+		circulationAmount: 5390,
+		accumulateAmount: 5502602,
+		descriptions: ['world.card.detail.one', 'world.card.detail.two'],
+		src: WorldCardSample,
+		imgSrc: {
+			en: {
+				titleImg: '',
+				releaseDateImg: '',
+				cardNameImg: '',
+			},
+			zh: {
+				titleImg: WorldCardTitle,
+				releaseDateImg: LastReleaseDate,
+				cardNameImg: WorldCard,
+			},
+		},
+	},
+];
