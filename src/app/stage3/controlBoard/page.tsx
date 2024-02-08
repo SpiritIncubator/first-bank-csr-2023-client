3d8fb4deb193827183249dc512409639023aec6e
moojing <starts789@gmail.com>
2024-02-08 21:55:09 +0800

    feat: add question button image.

--- src/app/stage3/controlBoard/page.tsx
+++ src/app/stage3/controlBoard/page.tsx
@@ -1,20 +1,21 @@
 'use client'
+import { useState } from 'react'
 import { Box } from '@mui/material';
-import { useState } from 'react';
-import Button from '@/app/_components/Button/Button';
+import Start from './_components/Start';
+import LookAtScreen from './_components/LookAtScreen';
 import { useSubscribe } from '@/app/hooks/useSubscribe';
-import Start from './_components/Start'
-import LookAtScreen from './_components/LookAtScreen'
+import Scene1QuestionList from './_components/Scene1QuestionList';
+import Image from 'next/image';
 import { STAGE3_ROOM } from '@/constants'
 
 enum STEPS {
   START = "START",
   LOOK_AT_SCREEN = "LOOK_AT_SCREEN",
-
+  SCENE1_QUESTION_LIST = "SCENE1_QUESTION_LIST",
 }
 
 export default function ControlBoard() {
-  const [currentStep, setCurrentStep] = useState(STEPS.START);
+  const [currentStep, setCurrentStep] = useState(STEPS.SCENE1_QUESTION_LIST);
   const { registerRoomHelper } = useSubscribe({ channel: 'subscribeChannel', room: 'stage3_controlBoard' });
   const { sendEvent } = registerRoomHelper();
 
@@ -26,7 +27,8 @@ export default function ControlBoard() {
 
   const screens = {
     [STEPS.START]: <Start onClickStart={onClickStart} />,
-    [STEPS.LOOK_AT_SCREEN]: <LookAtScreen />
+    [STEPS.LOOK_AT_SCREEN]: <LookAtScreen />,
+    [STEPS.SCENE1_QUESTION_LIST]: <Scene1QuestionList />
   }
   return (
     <Box boxSizing="border-box" width="2732px" height="2048px" position="relative" >
