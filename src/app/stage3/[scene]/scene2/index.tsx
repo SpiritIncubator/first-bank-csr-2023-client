bae3b7429e71d3bebf4c9cc862814dbccf218e88
Stan <tonytonitone6@gmail.com>
2024-02-14 02:49:49 +0800

    feat: scene page add fadein

--- src/app/stage3/[scene]/scene2/index.tsx
+++ src/app/stage3/[scene]/scene2/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Lottie from 'lottie-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+import FadeIn from '@/app/_components/Transitions/FadeIn';
 import { ConversationContext } from '../../layout'
 import lionAnimationData from '../../animationData/leo_2-11_normal_smile1.json';
 import Scene2BgImg from '../../assets/scene2-bg.svg';
@@ -17,7 +18,7 @@ const Scene2Page = () => {
   const currentPhaseInfo = ConversationContext.useSelector(state => state.context.phase);
   const action = ConversationContext.useActorRef();
   const isInitialDialog1 = currentPhaseInfo.round === 0 && currentPhaseInfo.level === 2;
-  console.log(currentPhaseInfo, 'currentPhaseInfo')
+  const [delayLoaded, setDelayLoaded] = useState(false);
   useEffect(() => {
     // TODO will receive
     if (isInitialDialog1) {
@@ -36,29 +37,42 @@ const Scene2Page = () => {
       }, 4000);
     }
   }, [currentPhaseInfo.round, currentPhaseInfo.level, action, router])
-  
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    timer = setTimeout(() => {
+      setDelayLoaded(true);
+    }, 1000);
+  }, []);
 
   return (
     <Box position="relative" width={2560} height={1440} overflow="hidden">
-      <Box position="absolute" top={370} left={250}>
-        <Image src={UnResolvedQuestion} alt="unresolved-question" />
-      </Box>
-      <Box position="absolute" top={800} left={570}>
-        <Image src={UnResolvedQuestion} alt="unresolved-question" />
-      </Box>
-      <Box position="absolute" top={625} right={800}>
-        <Image src={UnResolvedQuestion} alt="unresolved-question" />
-      </Box>
-      <Image src={Scene2BgImg} alt="scene2" priority />
-      <Box position="absolute" top={0} left={0}>
-        <Image src={DialogBg} alt="dialog" />
-      </Box>
-      <Box sx={{transform: 'translateX(-50%)'}} position="absolute" bottom={0} left="50%">
-        <Image src={isInitialDialog1 ? Fake1Img : Fake2Img} alt="fake1" />
-      </Box>
-      <Box position="absolute" right={0} bottom={-150}>
-        <Lottie animationData={lionAnimationData} loop />
-      </Box>
+      {delayLoaded && (
+        <FadeIn>
+          <Box position="absolute" top={370} left={250}>
+            <Image src={UnResolvedQuestion} alt="unresolved-question" />
+          </Box>
+          <Box position="absolute" top={800} left={570}>
+            <Image src={UnResolvedQuestion} alt="unresolved-question" />
+          </Box>
+          <Box position="absolute" top={625} right={800}>
+            <Image src={UnResolvedQuestion} alt="unresolved-question" />
+          </Box>
+          <Box position="absolute" top={0} left={0}>
+            <Image src={DialogBg} alt="dialog" />
+          </Box>
+          <Box position="absolute" right={0} bottom={-150}>
+            <Lottie animationData={lionAnimationData} loop />
+          </Box>
+          <Box sx={{ transform: 'translateX(-50%)' }} position="absolute" bottom={0} left="50%">
+            <Image src={isInitialDialog1 ? Fake1Img : Fake2Img} alt="fake1" />
+          </Box>
+        </FadeIn>
+      )}
+      <FadeIn>/
+        <Image src={Scene2BgImg} alt="scene2" priority />
+      </FadeIn>
     </Box>
   )
 }
