48f97f9e88120b2ed0728517bf884e7eb3cef25a
moojing <starts789@gmail.com>
2024-02-19 01:27:10 +0800

    feat: add answer button image.

--- src/app/stage3/controlBoard/_components/LookAtScreen.tsx
+++ src/app/stage3/controlBoard/_components/LookAtScreen.tsx
@@ -1,16 +1,15 @@
 'use client'
 import { Box } from '@mui/material';
-import Button from '@/app/_components/Button/Button';
-import { useSubscribe } from '@/app/hooks/useSubscribe';
 import Image from 'next/image';
+import LookAtScreenFullSize from '@/app/stage3/assets/controlBoard/stage3_look_at_screen_full_size.svg';
 
 
 
 export default function Start() {
   return <Box boxSizing="border-box" width="2732px" minHeight="2048px" position="relative">
     <Image
-      src="/assets/stage3/stage3_look_at_screen.svg"
-      alt="stage3_look_at_screen"
+      src={LookAtScreenFullSize}
+      alt="stage3_look_at_screen_full"
       fill />
-  </Box >
+  </Box>
 } 
--- src/app/stage3/controlBoard/page.tsx
+++ src/app/stage3/controlBoard/page.tsx
@@ -6,6 +6,7 @@ import LookAtScreen from './_components/LookAtScreen';
 import { useSubscribe } from '@/app/hooks/useSubscribe';
 import Scene1QuestionList from './_components/Scene1QuestionList';
 import Scene2QuestionList from './_components/Scene2QuestionList';
+import AnswerZone from './_components/AnswerZone';
 import { SOCKET_EVENTS } from '@/app/stage3/constants';
 import { STAGE3_ROOM } from '@/constants'
 
@@ -14,10 +15,11 @@ enum STEPS {
   LOOK_AT_SCREEN = "LOOK_AT_SCREEN",
   // SCENE1_QUESTION_LIST = "SCENE1_QUESTION_LIST",
   SCENE2_QUESTION_LIST = "SCENE2_QUESTION_LIST",
+  ANSWER_ZONE = "ANSWER_ZONE"
 }
 
 export default function ControlBoard() {
-  const [currentStep, setCurrentStep] = useState(STEPS.SCENE2_QUESTION_LIST);
+  const [currentStep, setCurrentStep] = useState(STEPS.ANSWER_ZONE);
   const { registerRoomHelper } = useSubscribe({ channel: 'subscribeChannel', room: STAGE3_ROOM });
   const { sendEvent, receivedEvent } = registerRoomHelper();
 
@@ -37,7 +39,8 @@ export default function ControlBoard() {
   const screens = {
     [STEPS.START]: <Start onClickStart={onClickStart} />,
     [STEPS.LOOK_AT_SCREEN]: <LookAtScreen />,
-    [STEPS.SCENE2_QUESTION_LIST]: <Scene2QuestionList />
+    [STEPS.SCENE2_QUESTION_LIST]: <Scene2QuestionList />,
+    [STEPS.ANSWER_ZONE]: <AnswerZone />
   }
   return (
     <Box boxSizing="border-box" width="2732px" height="2048px" position="relative" >
