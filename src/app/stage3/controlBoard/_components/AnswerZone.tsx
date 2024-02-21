deb01cfea81079b514d63fb55cd167fb8bb84aee
moojing <starts789@gmail.com>
2024-02-21 22:14:47 +0800

    feat: add lion successfully solve image.

--- src/app/stage3/controlBoard/_components/AnswerZone.tsx
+++ src/app/stage3/controlBoard/_components/AnswerZone.tsx
@@ -1,29 +1,26 @@
 import { Box } from '@mui/material';
 import Image from 'next/image';
-import LookAtScreen from '@/app/stage3/assets/controlBoard/stage3_look_at_screen.svg';
+import LookAtScreen from '@/app/stage3/assets/controlBoard/stage3_lion_look_at_screen.svg';
 
 export default function AnswerZone() {
+	return (
+		<Box
+			width="2732px"
+			minHeight="2048px"
+			sx={{
+				backgroundImage: 'url(/assets/stage3/background.svg)',
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				pt: '205px',
+			}}>
+			<Box mx="auto" mb="75px" position="relative" width="1854px" height="462px" textAlign="center">
+				<Image src={LookAtScreen} alt="stage3_lion_look_at_screen" fill />
+			</Box>
 
-  return <Box width="2732px" minHeight="2048px" sx={{
-    backgroundImage: "url(/assets/stage3/background.svg)",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    pt: "205px"
-  }}>
-    <Box mx="auto" mb="75px" position="relative" width="1854px" height="462px" textAlign="center">
-      <Image
-        src={LookAtScreen}
-        alt="stage3_look_at_screen"
-        fill />
-    </Box>
-
-    <Box width="866px" height="650px" bgcolor="gray" mb="100px">
-
-    </Box>
-
-
-  </Box>
+			<Box width="866px" height="650px" bgcolor="gray" mb="100px"></Box>
+		</Box>
+	);
 }
--- src/app/stage3/controlBoard/page.tsx
+++ src/app/stage3/controlBoard/page.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { useEffect, useState } from 'react'
+'use client';
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import Start from './_components/Start';
 import LookAtScreen from './_components/LookAtScreen';
@@ -8,48 +8,52 @@ import Scene1QuestionList from './_components/Scene1QuestionList';
 import Scene2QuestionList from './_components/Scene2QuestionList';
 import AnswerZone from './_components/AnswerZone';
 import { SOCKET_EVENTS } from '@/app/stage3/constants';
-import { STAGE3_ROOM } from '@/constants'
+import { STAGE3_ROOM } from '@/constants';
 
 enum STEPS {
-  START = "START",
-  LOOK_AT_SCREEN = "LOOK_AT_SCREEN",
-  // SCENE1_QUESTION_LIST = "SCENE1_QUESTION_LIST",
-  SCENE2_QUESTION_LIST = "SCENE2_QUESTION_LIST",
-  ANSWER_ZONE = "ANSWER_ZONE"
+	START = 'START',
+	LOOK_AT_SCREEN = 'LOOK_AT_SCREEN',
+	// SCENE1_QUESTION_LIST = "SCENE1_QUESTION_LIST",
+	SCENE2_QUESTION_LIST = 'SCENE2_QUESTION_LIST',
+	ANSWER_ZONE = 'ANSWER_ZONE',
 }
 
 export default function ControlBoard() {
-  const [currentStep, setCurrentStep] = useState(STEPS.START);
-  const { registerRoomHelper } = useSubscribe({ channel: 'subscribeChannel', room: STAGE3_ROOM });
-  const { sendEvent, receivedEvent } = registerRoomHelper();
+	const [currentStep, setCurrentStep] = useState(STEPS.ANSWER_ZONE);
+	const { registerRoomHelper } = useSubscribe({ channel: 'subscribeChannel', room: STAGE3_ROOM });
+	const { sendEvent, receivedEvent } = registerRoomHelper();
 
-  const onClickStart = () => {
-    sendEvent({ messageType: SOCKET_EVENTS.START });
-    setCurrentStep(STEPS.LOOK_AT_SCREEN);
-  }
+	const onClickStart = () => {
+		sendEvent({ messageType: SOCKET_EVENTS.START });
+		setCurrentStep(STEPS.LOOK_AT_SCREEN);
+	};
 
-  useEffect(() => {
-    receivedEvent(({ messageType }) => {
-      if (messageType === SOCKET_EVENTS.READY_FOR_QUEST) {
-        setCurrentStep(STEPS.SCENE2_QUESTION_LIST);
-      }
+	useEffect(() => {
+		receivedEvent(({ messageType }) => {
+			if (messageType === SOCKET_EVENTS.READY_FOR_QUEST) {
+				setCurrentStep(STEPS.SCENE2_QUESTION_LIST);
+			}
 
-      if (messageType === SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START) {
-        // @todo: get into answer zone 
-        console.log('SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START :', SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START);
-      }
-    })
-  }, [receivedEvent])
+			if (messageType === SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START) {
+				// @todo: get into answer zone
+				console.log(
+					'SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START :',
+					SOCKET_EVENTS.QUEST_RAINWATER_QUIZ1_START,
+				);
+				setCurrentStep(STEPS.ANSWER_ZONE);
+			}
+		});
+	}, [receivedEvent]);
 
-  const screens = {
-    [STEPS.START]: <Start onClickStart={onClickStart} />,
-    [STEPS.LOOK_AT_SCREEN]: <LookAtScreen />,
-    [STEPS.SCENE2_QUESTION_LIST]: <Scene2QuestionList />,
-    [STEPS.ANSWER_ZONE]: <AnswerZone />
-  }
-  return (
-    <Box boxSizing="border-box" width="2732px" height="2048px" position="relative" >
-      {screens[currentStep]}
-    </Box>
-  )
+	const screens = {
+		[STEPS.START]: <Start onClickStart={onClickStart} />,
+		[STEPS.LOOK_AT_SCREEN]: <LookAtScreen />,
+		[STEPS.SCENE2_QUESTION_LIST]: <Scene2QuestionList />,
+		[STEPS.ANSWER_ZONE]: <AnswerZone />,
+	};
+	return (
+		<Box boxSizing="border-box" width="2732px" height="2048px" position="relative">
+			{screens[currentStep]}
+		</Box>
+	);
 }
