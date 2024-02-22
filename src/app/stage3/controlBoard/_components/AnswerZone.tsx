d5487e5fc28296817db71f2665cae47d6338ae40
moojing <starts789@gmail.com>
2024-02-23 01:20:00 +0800

    feat: change image in Answer zone.

--- src/app/stage3/controlBoard/_components/AnswerZone.tsx
+++ src/app/stage3/controlBoard/_components/AnswerZone.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import { useState } from 'react';
 import Image from 'next/image';
-import LookAtScreen from '@/app/stage3/assets/controlBoard/stage3_lion_look_at_screen.svg';
+import CLICK_BUTTON_BELOW from '@/app/stage3/assets/controlBoard/stage3_click_button_below.svg';
 import {
 	quizEventAnswerMapping,
 	QuestNames,
@@ -79,7 +79,7 @@ export default function AnswerZone({
 				pt: '205px',
 			}}>
 			<Box mx="auto" mb="75px" position="relative" width="1854px" height="462px" textAlign="center">
-				<Image src={LookAtScreen} alt="stage3_lion_look_at_screen" fill />
+				<Image src={CLICK_BUTTON_BELOW} alt="stage3_lion_look_at_screen" fill />
 			</Box>
 
 			<Box width="866px" height="650px" mb="100px" position="relative">
