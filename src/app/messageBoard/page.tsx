691d9ff803cda3c71628dea77e9c91d86e092e63
moojing <starts789@gmail.com>
2024-02-28 00:49:27 +0800

    feat: add animation to stage5 message wall.

--- src/app/messageBoard/page.tsx
+++ src/app/messageBoard/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Box from '@mui/material/Box';
 import styled from '@mui/material/styles/styled';
@@ -13,61 +13,75 @@ import title from './assets/title.svg';
 import colors from '@/constants/colors';
 import CardQueueAnimation from '../_components/Transitions/CardQueued';
 import { MessageCardType } from '@/app/messageBoard/hooks';
+import LionWriteAnimation from '@/lottieAnimations/stage5-lion-write.json';
+import LionBirdIn from '@/lottieAnimations/stage5-lion-bird-in.json';
+import Lottie from 'lottie-react';
 
 const StyledBox = styled(Box)`
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
+	::-webkit-scrollbar {
+		display: none;
+	}
+`;
 
 const Page = () => {
-  const { messages, loaded } = useMessageBoard();
-  console.log('messages :', messages);
-  const { containerRef, value, handleChangeBarOfValue } = useScrollBar({ loaded });
+	const { messages, loaded } = useMessageBoard();
+	console.log('messages :', messages);
+	const { containerRef, value, handleChangeBarOfValue } = useScrollBar({ loaded });
 
-  return (
-    <Box display="flex" flexDirection="column" alignItems="center" px={23.75} pt={15} bgcolor={colors.ivory}>
-      <Image src={BgIcon} alt="bg" />
-      <Box mt={9}>
-        <Image src={title} alt="title" />
-      </Box>
-      <Box position="relative" width="100%">
-        <StyledBox
-          display="flex"
-          flexWrap="wrap"
-          gap={7.5}
-          mt={5}
-          maxHeight={2240}
-          overflow="auto"
-          ref={containerRef}
-        >
-          {!loaded ?
-            <Box
-              height="2000px"
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
+	return (
+		<Box
+			display="flex"
+			flexDirection="column"
+			alignItems="center"
+			px={23.75}
+			pt={15}
+			bgcolor={colors.ivory}>
+			<Lottie animationData={LionBirdIn} style={{ width: '100%', height: 'auto' }} />
+			<Box mt={9}>
+				<Image src={title} alt="title" />
+			</Box>
+			<Box position="relative" width="100%">
+				<StyledBox
+					display="flex"
+					flexWrap="wrap"
+					gap={7.5}
+					mt={5}
+					maxHeight={2240}
+					overflow="auto"
+					ref={containerRef}>
+					{!loaded ? (
+						<Box
+							height="2000px"
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								width: '100%',
+							}}>
+							<CircularProgress size="100px" color="inherit" />
+						</Box>
+					) : (
+						<CardQueueAnimation<MessageCardType>
+							items={messages}
+							keyExtractor={(item, index) => index + item.name}
+							renderItem={(item, index) => (
+								<MessageCard
+									key={index}
+									bgColor={item.color}
+									isTop={item.keepTop}
+									content={item.message}
+									name={item.name}
+								/>
+							)}
+						/>
+					)}
+				</StyledBox>
+				<Box position="absolute" right={-100} top={150} height={600}>
+					<ScrollBar value={value} onHandleScrollBar={handleChangeBarOfValue} />
+				</Box>
+			</Box>
+		</Box>
+	);
+};
 
-              }}
-            >
-              <CircularProgress size="100px" color="inherit" />
-            </Box> :
-            <CardQueueAnimation<MessageCardType>
-              items={messages}
-              keyExtractor={(item, index) => index + item.name}
-              renderItem={(item, index) => (<MessageCard key={index} bgColor={item.color} isTop={item.keepTop} content={item.message} name={item.name} />)}
-            />
-          }
-
-        </StyledBox>
-        <Box position="absolute" right={-100} top={150} height={600}>
-          <ScrollBar value={value} onHandleScrollBar={handleChangeBarOfValue} />
-        </Box>
-      </Box >
-    </Box >
-  )
-}
-
-export default Page
+export default Page;
