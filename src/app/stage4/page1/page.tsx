140111f69709e6b5caa6d1a76a23cc11aec0eb7b
moojing <starts789@gmail.com>
2024-02-28 17:31:44 +0800

    feat: finish page1.

--- src/app/stage4/page1/page.tsx
+++ src/app/stage4/page1/page.tsx
@@ -9,135 +9,51 @@ import {
 import Image from 'next/image';
 import { useCallback } from 'react';
 
-import DialogImg from '../_assets/stage4-dialog.svg';
 import FadeIn from '@/app/_components/Transitions/FadeIn';
 import FadeInOnView from '@/app/_components/Transitions/FadeInOnView';
 import ScrollBar, {useScrollBar} from '@/app/_components/ScrollBar';
 import NavBar from '../_components/NavBar/NavBar';
-
-const CASE_DATA = [
-  {
-    title: '綠色產業',
-    imageUrl: '',
-    "description": `支持能源技術服務產業(Energy Service Companies；簡稱 ESCO產業)發展，節電、節水、節油等工程及設備貸款`
-  },
-  {
-    title: '綠色產業',
-    imageUrl: '',
-    "description": `支持能源技術服務產業(Energy Service Companies；簡稱 ESCO產業)發展，節電、節水、節油等工程及設備貸款`
-  },
-  {
-    title: '綠色產業',
-    imageUrl: '',
-    "description": `支持能源技術服務產業(Energy Service Companies；簡稱 ESCO產業)發展，節電、節水、節油等工程及設備貸款`
-  }
-]
+import { useTranslation } from '@/app/_locales/hooks/useTranslation';
+import { LANGUAGE_TYPE } from '@/types';
+import Stage4Page1MainEN from '../_assets/stage4-page1-main-en.svg';
+import Stage4Page1MainZH from '../_assets/stage4-page1-main-zh.svg';
+import Stage4Page1TitleEN from '../_assets/stage4-page1-title-en.svg';
+import Stage4Page1TitleZH from '../_assets/stage4-page1-title-zh.svg';
+import Stage4ScrollEN from '../_assets/stage4-scroll-en.svg';
+import Stage4ScrollZH from '../_assets/stage4-scroll-zh.svg';
+ 
 
 //EsgConsumerLoans
 export default function Page1() {
+  const { i18n } = useTranslation();
+  const isEN = i18n.language === LANGUAGE_TYPE.EN;
   const { value, handleChangeBarOfValue, containerRef } = useScrollBar({});
 
   const MemorizedImage = useCallback(() => {
-    return <Image src={DialogImg} alt="dialog" />
+    return <Image src={isEN ? Stage4ScrollEN:Stage4ScrollZH } alt="dialog" />
   }, []);
 
   return (
     <Box px="210px" py="132px" mr="auto" mb="320px" position="relative" ref={containerRef} height={2160} overflow="scroll">
       <Box maxWidth="2404px" >
-        <FadeIn>
-          <Box width="1342px" height="154px" position="relative" mb="60px" textAlign="left" >
+      <FadeIn>
+          <Box width="1970px" height="349px" position="relative" mb="120px" textAlign="left" >
             <Image
               fill
-              src="/assets/esg-consumer-loans-title.svg"
+              src={isEN? Stage4Page1TitleEN:Stage4Page1TitleZH}
               alt="colorPicker_select_color"
             />
           </Box>
-
-          <Box fontSize="52px" lineHeight="98.8px" mb="120px" width="1840px">
-            為協助企業降低生產或服務過程中產生之汙染及資源浪費，針對綠色產業及綠色企業提供之優惠貸款。
-          </Box>
-        </FadeIn>
-
-        <Grid container spacing={2} sx={{ width: '100%', p: 2 }}  >
-          {CASE_DATA.map((caseData, index) => {
-            const { title, description } = caseData;
-            return (<Grid item
-              xs={12} sm={4} key={index} display="flex">
-              <FadeIn delay={0.3 + index * 0.2}
-                display="flex"
-                justifyContent="center"
-                flexDirection="column"
-                alignItems="center">
-                <Box
-                  display="flex"
-                  justifyContent="center"
-                  width="100%"
-                >
-                  <Paper
-                    elevation={0}
-                    sx={{
-                      bgcolor: '#E9E3D8',
-                      height: '420px',
-                      width: '420px',
-                      mb: '40px',
-                    }} />
-
-                </Box>
-                <Paper
-                  elevation={0}
-                  sx={{
-                    bgcolor: 'darkgrey',
-                    width: "500px",
-                    height: '160px',
-                    padding: '20px',
-                    mb: "40px"
-                  }}
-                >
-                  <Typography variant="body1" color="white">
-                    {title}
-                  </Typography>
-                </Paper>
-                <Box sx={{
-                  color: "var(--brown4, #594A39)",
-                  width: "720px",
-                  fontSize: "46px",
-                  fontStyle: "normal",
-                  fontWeight: 500,
-                  lineHeight: "160%",
-                  letterSpacing: "3.68px"
-                }}>
-                  {description}
-                </Box>
-              </FadeIn>
-            </Grid>)
-          })}
-        </Grid>
-
-        <Box sx={{ mt: '96px' }}>
-
-          <FadeInOnView delay={0.5}>
-            <Paper
-              sx={{
-                bgcolor: 'lightgrey',
-                width: "100%",
-                height: '1200px',
-                mb: "160px"
-              }} />
-          </FadeInOnView>
-          <FadeInOnView delay={0.5}>
-            <Paper
-              sx={{
-                bgcolor: 'lightgrey',
-                width: "100%",
-                height: '1600px',
-                mb: "160px"
-              }} />
-          </FadeInOnView>
-        </Box>
-
-        <Box position="fixed" right={163} top={378} height={600}>
-          <ScrollBar value={value} onHandleScrollBar={handleChangeBarOfValue} />
-        </Box>
+          </FadeIn>
+          <FadeIn delay={0.5}>
+            <Box width="100%" height="100%" position="relative" display="flex" justifyContent="center" alignItems="center">
+              <Image
+                src={isEN ? Stage4Page1MainEN :Stage4Page1MainZH }
+                alt="Stage4Page1Main"
+              />
+            </Box>
+          </FadeIn>
+          
       </Box>
       <NavBar dialogContent={MemorizedImage} />
     </Box >
