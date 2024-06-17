359efa55cdc0ffea3e5fd64ef7e9fe5dc06959ad
Stan <tonytonitone6@gmail.com>
2024-06-16 20:11:42 -0400

    feat: add preious, next button and animation

--- src/app/stage2/_components/Instructions.tsx
+++ src/app/stage2/_components/Instructions.tsx
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
 import { Box, Typography, Button, Modal } from '@mui/material';
+import Image from 'next/image';
 
 import BackToHome from '@/app/_assets/images/backToHome.svg';
 import FootButton from '@/app/_assets/images/FootButton.svg';
-import Image from 'next/image';
+import NextButton from '@/assets/back_right.svg';
+import NextActiveButton from '@/assets/back_right_active.svg';
+import PreviousButton from '@/assets/back_left.svg';
+import PreviousActiveButton from '@/assets/back_left_active.svg';
+import ImageButton from '@/app/_components/ImageButton/ImageButton';
+import FadeInHorizontal from '@/app/_components/Transitions/FadeInHorizontal';
 
-const Instructions = ({ onClickGoHome }: { onClickGoHome: () => void }) => {
+type PageType = {
+	title: string;
+	imgPath: string;
+};
+
+const IntroductionPages: PageType[] = [
+	{
+		title: 'Introduction1',
+		imgPath: '',
+	},
+	{
+		title: 'Introduction2',
+		imgPath: '',
+	},
+	{
+		title: 'Introduction3',
+		imgPath: '',
+	},
+	{
+		title: 'Introduction4',
+		imgPath: '',
+	},
+	{
+		title: 'Introduction5',
+		imgPath: '',
+	},
+];
+
+const Instructions = ({ onClickGoHome, title, imgPath }: { onClickGoHome: () => void, } & PageType) => {
 	return (
 		<Box
 			display="flex"
@@ -19,7 +53,7 @@ const Instructions = ({ onClickGoHome }: { onClickGoHome: () => void }) => {
 				<Image src={BackToHome} alt="back to home" width={277} height={96} />
 			</Box>
 			<Box width="1600px" height="2800px" bgcolor="gray">
-				Pic
+				{title}
 			</Box>
 		</Box>
 	);
@@ -27,9 +61,24 @@ const Instructions = ({ onClickGoHome }: { onClickGoHome: () => void }) => {
 
 export const InstructionButton: React.FC = () => {
 	const [open, setOpen] = useState(false);
-	console.log('open :', open);
+	const [pages, setPages] = useState(0);
+	const page = IntroductionPages[pages];
+	const hasPreviousButton = pages !== 0;
+	const hasNextButton = pages !== IntroductionPages.length - 1;
 
 	const toggleOpen = () => setOpen(prev => !prev);
+
+	function jumpToNextPage() {
+		if (hasNextButton) {
+			setPages(prev => prev + 1);
+		}
+	}
+
+	function jumpToPreviousPage() {
+		if (hasPreviousButton) {
+			setPages(prev => prev - 1);
+		}
+	}
 
 	return (
 		<>
@@ -45,7 +94,7 @@ export const InstructionButton: React.FC = () => {
 
 			<Box
 				position="fixed"
-				zIndex="9999999"
+				zIndex="1000"
 				bgcolor="white"
 				right={open ? '0' : '-100%'}
 				top="0"
@@ -57,7 +106,31 @@ export const InstructionButton: React.FC = () => {
 				boxSizing="border-box"
 				// style={{ transition: 'right 0.5s ease-in-out' }}
 			>
-				<Instructions onClickGoHome={toggleOpen} />
+				<Box zIndex={9999} height="100%" display="flex" position="absolute" left={0} alignItems="center">
+					{hasPreviousButton && (
+						<ImageButton
+							width="143px"
+							height="283px"
+							src={PreviousButton}
+							activeImageSrc={PreviousActiveButton}
+							onClick={jumpToPreviousPage}
+						/>
+					)}
+				</Box>
+				<FadeInHorizontal direction='rtl' key={`${pages}/description`}>
+					<Instructions onClickGoHome={toggleOpen} {...page} />
+				</FadeInHorizontal>
+				{hasNextButton && (
+					<Box zIndex={9999} height="100%" display="flex" position="absolute" right={0} alignItems="center">
+						<ImageButton
+							width="143px"
+							height="283px"
+							src={NextButton}
+							activeImageSrc={NextActiveButton}
+							onClick={jumpToNextPage}
+						/>
+					</Box>
+				)}
 			</Box>
 		</>
 	);
