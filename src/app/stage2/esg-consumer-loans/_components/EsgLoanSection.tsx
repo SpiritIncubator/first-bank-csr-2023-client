4336b4a73835b49b844d6cbb2adba43371c737cb
moojing <starts789@gmail.com>
2025-08-19 17:08:31 +0100

    feat: update bird image in esg page

--- src/app/stage2/esg-consumer-loans/_components/EsgLoanSection.tsx
+++ src/app/stage2/esg-consumer-loans/_components/EsgLoanSection.tsx
@@ -137,12 +137,14 @@ export default function EsgLoanSection() {
 								letterSpacing: '8.4px',
 								marginTop: '-60px',
 							}}>
-							{isEN ? ESG_SECTION_METRICS.greenConsumerLoan.balanceAmount.en : formatNumberWithCommas(ESG_SECTION_METRICS.greenConsumerLoan.balanceAmount.zh)}
+							{isEN
+								? ESG_SECTION_METRICS.greenConsumerLoan.balanceAmount.en
+								: formatNumberWithCommas(ESG_SECTION_METRICS.greenConsumerLoan.balanceAmount.zh)}
 						</Box>
 					</Box>
 				</Box>
 				<Box flex="1" position="relative" top="150px" width="660px" height="580px">
-					<Image fill src={`/assets/stage2/2023-score-${lang}.svg`} alt="2022 score" />
+					<Image fill src={`/assets/stage2/2024-score-${lang}.svg`} alt="2022 score" />
 				</Box>
 			</Box>
 		</FadeInHorizontal>
